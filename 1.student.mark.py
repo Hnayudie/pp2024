2d662610f8741a8b2610d597bf35b68d51f44d45
La Duy Anh <anhld.22bi13016@usth.edu.vn>
2024-01-04 14:31:36 +0700

    Labwork 1

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,19 +1,21 @@
 def input_student_info ():
-    num_student = int(input("Enter the number of student: "))
-    student()
-    for i in range (num_student) :
+    num_students = int(input("Enter the number of student: "))
+    students=[]
+    for i in range (num_students) :
         student_name = input(f"Enter Student's Name {i+1}: ")
         student_id = input(f"Enter Student's ID {i+1}: ")
         student_dob = input(f"Enter Student DOB {i+1}: ")
-        stuin4 = (student_name, student_id, student_dob)
-        student.append(stuin4)
+        student.append(student_name, student_id, student_dob)
+    return students
         
 def input_course_info():
-    num_course = int(input("Enter the number of "))
-    course()
-    for i in range (num_course):
+    num_courses = int(input("Enter the number of "))
+    courses=[]
+    for i in range (num_courses):
         course_name = input(f"Enter Course's Name {i+1}: ")
         course_id = input(f"Enter Course's ID {i+1}: ")
+        course.append(course_name, course_id)
+    return courses
         
 def input_student_mark():
     mark=[]
