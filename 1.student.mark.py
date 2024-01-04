735cf0f1bf2ab9d49ff1f4b2cfe1ec8b97696565
La Duy Anh <anhld.22bi13016@usth.edu.vn>
2024-01-04 13:32:02 +0700

    Labwork 1

---
+++ 1.student.mark.py
@@ -0,0 +1,19 @@
+def input_student_info ():
+    num_student = int(input("Enter the number of student: "))
+    student()
+    for i in range (number_student) :
+        student_name = input(f"Enter Student's Name {i+1}: ")
+        student_id = input(f"Enter Student's ID {i+1}: ")
+        student_dob = input(f"Enter Student DOB {i+1}: ")
+        
+def input_course_info():
+    num_course = int(input("Enter the number of "))
+    course()
+    for i in range (number_course):
+        course_name = input(f"Enter Course's Name {i+1}: ")
+        course_id = input(f"Enter Course's ID {i+1}: ")
+        
+        
+    
+    
+    
