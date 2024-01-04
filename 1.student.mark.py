70da36f984711b476226b92a419ad141a595b555
La Duy Anh <anhld.22bi13016@usth.edu.vn>
2024-01-04 16:23:49 +0700

    Labwork 1

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -16,3 +16,27 @@ def list_students(students):
 def list_courses(courses):
     for course in courses:
         print(f"ID: {course['id']}, Name: {course['name']}")
+        
+def main():
+    # Input function: 
+    num_students = int(input("Enter number of students: "))
+    num_courses = int(input("Enter number of courses: "))
+    
+    # Students and courses List
+    students = []
+    courses = []
+    
+    # Input Students and Courses info
+    for _ in range(num_students):
+        student_name = input("Enter Student Name: ")
+        student_id = int(input("Enter Student ID: "))
+        student_dob = input("Enter Student DOB: ")
+        marks = {course['id']: None for course in courses}
+        students.append({'name': student_name, 'id': student_id, 'dob': student_dob})
+        
+    for _ in range(num_courses):
+        course_name = input("Enter Course Name: ")
+        course_id = int(input("Enter Course ID: "))
+        courses.append({'name': course_name, 'id': course_id})
+        
+        
