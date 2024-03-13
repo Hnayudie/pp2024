0be93e5af46ce05b2df106d64b9ef367d4c7b76f
La Duy Anh <anhld.22bi13016@usth.edu.vn>
2024-03-13 11:21:41 +0700

    Labwork 4

--- pw4/output.py
+++ pw4/output.py
@@ -1,13 +1,10 @@
 import curses
 
 def display_courses(courses):
-    # Implement curses-based display of courses
     pass
 
 def display_students(students):
-    # Implement curses-based display of students
     pass
 
 def display_marks(mark_sheet):
-    # Implement curses-based display of marks
     pass
