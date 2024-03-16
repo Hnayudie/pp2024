04c8e9b749d0f2f6d3109a35df40bed50d897c84
La Duy Anh <anhld.22bi13016@usth.edu.vn>
2024-03-16 08:59:09 +0700

    Labwork 4

--- pw4/main.py
+++ pw4/main.py
@@ -12,7 +12,7 @@ def main():
     courses = input_courses(num_courses)
 
     mark_sheet = MarkSheet(students, courses)
-    mark_sheet.input_mark()  # Corrected method name
+    mark_sheet.input_mark()  
 
     display_courses(courses)
     display_students(students)
